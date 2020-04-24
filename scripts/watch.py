41ad8fd2a525033be4d4ad97f3fff784b303bd6a
Piotr <szlachcic49er@gmail.com>
2020-04-24 15:59:15 +0200

    add rover status publisher

--- scripts/watch.py
+++ scripts/watch.py
@@ -1,30 +1,15 @@
 #!/usr/bin/env python
 import math
-import time
 import rospy
-from geometry_msgs.msg import Twist
+from std_msgs.msg import String
 from ar_track_alvar_msgs.msg import AlvarMarkers
 from tf.transformations import euler_from_quaternion
 
-position=None
-angle=None
-
-last_callback=0.0
-refresh_time=5
-
-is_available=False
-
-
 def callback_marker(data):
-	global position
-	global angle
-
-	global last_callback
-	global is_available
-
-	if time.time()-last_callback>refresh_time:
-		is_available=False
-	else: is_available=True
+	global height
+	global width
+	global thickness
+	msg=String()
 
 	try:
 		position=data.markers[0].pose.pose.position
@@ -33,22 +18,34 @@ def callback_marker(data):
 		orientation_list = [orientation_quat.x, orientation_quat.y, orientation_quat.z, orientation_quat.w]
 		orientation = euler_from_quaternion(orientation_list)
 		angle=orientation[2]
-		print (orientation, angle)
 
-		last_callback=time.time()
+		if abs(position.x)<=width/2.0 and abs(position.y)<=height/2.0: msg="in"
+		elif abs(position.x)>=(width/2.0)+thickness or abs(position.y)>=(height/2.0)+thickness: msg="out"
+		else: msg="controlled"
 
-	except: return
+		pub_status.publish(msg)
+
+	except: 
+		msg="unknown"
+		pub_status.publish(msg)
 
 
-if __name__ == "__main__":
-	try:
-		rospy.init_node('go_to_goal')
-		pub_cmd = rospy.Publisher("big_brother/cmd_vel", Twist, queue_size=10)
-		sub_marker = rospy.Subscriber('ar_pose_marker', AlvarMarkers, callback_marker)
-	except rospy.ROSInterruptException as e:
-		rospy.logerr(e)
 
-	rospy.spin()
+
+height = rospy.get_param("/height", 0.3)
+width = rospy.get_param("/width", 0.3)
+thickness = rospy.get_param("/thickness", 0.1)
+rospy.loginfo("Int: %s,Int: %s,Int: %s", height , width, thickness)
+
+
+try:
+	rospy.init_node('big_brother_watch')
+	pub_status = rospy.Publisher("big_brother/leo_status", String, queue_size=10)
+	sub_marker = rospy.Subscriber('ar_pose_marker', AlvarMarkers, callback_marker)
+except rospy.ROSInterruptException as e:
+	rospy.logerr(e)
+
+rospy.spin()
 
 
 	
